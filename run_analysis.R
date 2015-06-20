1a8d370c8b89276e8a87892ff0da1b7857560b96
akhilk13 <kansal_akhil@yahoo.com>
2015-06-21 03:41:05 +0900

    Course Project
    Commit 1

---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+#include the plyr package which will be used later
+library(plyr)
+
+# Step 1
+# Merge the training and test sets to create one data set
+###############################################################################
+
+#All the data should be present in the same folder, un-compressed and without names altered.
+
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+
+# create 'x' data set
+x_data <- rbind(x_train, x_test)
+
+# create 'y' data set
+y_data <- rbind(y_train, y_test)
+
+# create 'subject' data set
+subject_data <- rbind(subject_train, subject_test)
+
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement
+###############################################################################
+
+features <- read.table("features.txt")
+
+# get indexes of only those columns with mean() or std() in their names
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subset the desired columns of x_data according to required indexes
+x_data <- x_data[, mean_and_std_features]
+
+# assign appropriate column names from features table
+names(x_data) <- features[mean_and_std_features, 2]
+# removes paranthesis from column names
+names(x_data) <- gsub("\\(|\\)", "", names(x_data))
+
+# Step 3
+# Use descriptive activity names to name the activities in the data set
+###############################################################################
+
+activities <- read.table("activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name of y_data to "activity"
+names(y_data) <- "activity"
+
+# Step 4
+# Appropriately label the data set with descriptive variable names
+###############################################################################
+
+# correct column name of subject_data to "subject"
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+
+# Step 5
+# Create a second, independent tidy data set with the average of each variable
+# for each activity and each subject
+###############################################################################
+
+# 66 <- 68 columns but last two (activity & subject)
+tidy_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
